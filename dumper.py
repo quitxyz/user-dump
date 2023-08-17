4d3df665c6180b656a82a29190c9f91b1ffb69be
quit <104299623+quitxyz@users.noreply.github.com>
2023-08-17 05:20:59 +0200

    Update dumper.py

--- dumper.py
+++ dumper.py
@@ -2,7 +2,7 @@ import json
 
 steamid=xxx
 
-quit=json.loads(open("usernames.json","r").read())["RandomUsernames"]
+quit=json.loads(open("usernames.json","r").read())["usernames"]
 
 v = steamid % 2147483647
 
